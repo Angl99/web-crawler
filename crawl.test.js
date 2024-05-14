700cfa0ec34ead2a96d2717cd9e9500e07b0e325
Angl99 <anggelplasencia@pursuit.org>
2024-05-14 12:27:19 -0400

    normalizeURL and tests

--- crawl.test.js
+++ crawl.test.js
@@ -8,3 +8,24 @@ test('normalizeURL strip protocol', () =>{
     // expecting the actual to equal the expected = passed test
     expect(actual).toEqual(expected)
 })
+
+test('normalizeURL strip trailing slash', () =>{
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL capitals', () =>{
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip http', () =>{
+    const input = 'http://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
